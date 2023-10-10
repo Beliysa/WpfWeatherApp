40550f7899586680dee327cd8b9d3908209ef203
Deniz TOPAY <deniztopay@gmail.com>
2023-10-10 04:51:06 +0300

    Add project files.

---
+++ WpfHelloAPP/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using WpfHelloAPP;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+
+
+namespace WpfHelloAPP
+{  
+    public partial class MainWindow : Window
+    {
+        private readonly WeatherService _weatherService = new WeatherService();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var MyConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+           
+            var apiKey = MyConfig.GetSection("AppSettings")["ApiKey"];
+           
+
+            Console.WriteLine(apiKey);
+       
+
+
+            string city = "London"; // Or get from user input
+            //string apiKey = "*********"; 
+
+            string weatherData = await _weatherService.GetWeatherDataAsync(city, apiKey);
+            Console.WriteLine(weatherData);
+            
+
+            // Display the weatherData in your application (e.g., in a text box or label)
+            // ...
+        }
+
+
+    }
+}
---
+++ WpfHelloAPP/WeatherService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace WpfHelloAPP
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetWeatherDataAsync(string city, string apiKey)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric");
+
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
